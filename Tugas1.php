91fad85707ca1e442cee2259a9d0e876dbcde4e6
AAProject <87751322+asmajati15@users.noreply.github.com>
2021-09-08 00:27:40 +0700

    Add files via upload

---
+++ Tugas1.php
@@ -0,0 +1,80 @@
+
+<html>
+    <head>
+        <title>Tugas 1</title>
+        <link rel="stylesheet" href="Tugas1.css">
+    </head>
+    <body>
+        
+        <div class="menu">
+            <a href="">Home</a>&nbsp;|
+            <a href="">Profile</a>&nbsp;|
+            <a href="">Contact</a>
+        </div>
+
+
+        <div class="banner">
+        </div>
+
+        <div class="body">
+            <div id="b_left">
+                <ul>Co-Founder of:
+                    <li>Rain Projet Team</li>
+                    <li>Permobilan Merch</li>
+                </ul>
+                <ul>Educational background:
+                    <li>SMP YPPI ArRahmah (2017-2020)</li>
+                    <li>SMK Negeri 4 Kota Bogor (2020-now)</li>
+                </ul>
+            </div>
+            <div id="b_right">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse iaculis lacus dapibus fringilla tincidunt.
+                    Nulla odio tortor, lacinia vel tempor luctus, aliquam eu 
+                    eros. Mauris risus neque, convallis sit amet pellentesque 
+                    varius, euismod nec orci. Quisque sollicitudin sit amet 
+                    leo a ultricies. Curabitur tempus rutrum elit, sit amet 
+                    porttitor tortor commodo fringilla. Donec urna lacus, 
+                    eleifend quis mauris sed, molestie egestas purus. Mauris 
+                    mattis nulla sed mi bibendum semper. In hac habitasse 
+                    platea dictumst. Aenean commodo dictum blandit. Duis 
+                    nisl augue, fermentum ac nibh quis, laoreet condimentum 
+                    lorem. Nulla id eleifend urna. Morbi cursus faucibus 
+                    nibh, ut molestie eros tempus nec. Sed dignissim justo 
+                    sed sem pretium, vel pharetra justo dignissim. Integer 
+                    cursus lorem ac condimentum suscipit.
+                </p>
+                <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. 
+                    Donec ut mauris est. Ut sit amet egestas dui. Nam hendrerit 
+                    enim id nibh rutrum tincidunt. Donec sit amet massa massa. 
+                    Morbi sodales dui eget elit dictum condimentum a quis tellus. 
+                    Maecenas et varius velit. Etiam porttitor nulla eget lorem 
+                    dapibus, vel imperdiet felis suscipit.
+                </p>
+                <p>Quisque vitae massa ac eros bibendum viverra sed eget urna. 
+                    Integer quis dolor hendrerit lectus sollicitudin consequat. 
+                    Integer in tellus in sapien scelerisque condimentum eget vitae 
+                    sapien. Sed commodo est in mi ornare rhoncus. Morbi viverra sit 
+                    amet ipsum at facilisis. Sed laoreet lectus pulvinar erat feugiat, 
+                    et ultricies lorem fringilla. Aliquam pretium malesuada justo vel 
+                    feugiat. Praesent varius quam sed arcu iaculis, non luctus dolor 
+                    euismod. Aliquam euismod lacus non rutrum ultrices. Sed quis 
+                    sodales nisi. Suspendisse ut neque nunc. Etiam convallis at 
+                    eros sit amet eleifend. Fusce at interdum ex, ut consequat elit.
+                </p>
+            </div>
+        </div>
+
+
+        <div class="footer">
+            <div id="f_left">
+            </div>
+            <div id="f_right">
+                <a href="">Home</a>&nbsp;|
+                <a href="">Profile</a>&nbsp;|
+                <a href="">Contact</a>
+            </div>
+        </div>
+
+    </body>
+</html>
